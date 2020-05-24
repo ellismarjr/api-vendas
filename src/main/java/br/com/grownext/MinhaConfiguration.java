ecb612502a601b6c7e812353d2af159809fc1cf2
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-05-24 13:42:49 -0300

    Profiles

--- src/main/java/br/com/grownext/MinhaConfiguration.java
+++ src/main/java/br/com/grownext/MinhaConfiguration.java
@@ -1,12 +1,20 @@
 package br.com.grownext;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("Rodando configuração de desenvolvimento");
+        };
     }
+
+
 }
