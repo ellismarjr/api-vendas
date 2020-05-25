2229f5fde0bcc1060f5920b8ec5bb9e410f1c2ff
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-05-24 21:55:04 -0300

    Working with EntityManager

--- src/main/java/br/com/grownext/VendasApplication.java
+++ src/main/java/br/com/grownext/VendasApplication.java
@@ -1,15 +1,13 @@
 package br.com.grownext;
 
 import br.com.grownext.domain.entity.Cliente;
-import br.com.grownext.domain.repositorio.Clientes;
-import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
+import br.com.grownext.domain.repositorio.ClientesRespositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,36 +17,36 @@ import java.util.List;
 @RestController
 public class VendasApplication {
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes) {
+    public CommandLineRunner init(@Autowired ClientesRespositorio clientesRespositorio) {
         return args -> {
             System.out.println("SALVANDO CLIENTES");
-            clientes.salvar(new Cliente("Ellismar"));
-            clientes.salvar(new Cliente("Cliente Junior"));
+            clientesRespositorio.salvar(new Cliente("Ellismar"));
+            clientesRespositorio.salvar(new Cliente("Cliente Junior"));
 
             System.out.println("LISTANDO CLIENTES");
-            List<Cliente> todosClientes = clientes.obterTodos();
+            List<Cliente> todosClientes = clientesRespositorio.obterTodos();
             todosClientes.forEach(System.out::println);
-
-            System.out.println("ATUALIZANDO CLIENTES");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado");
-                clientes.atualizar(c);
-            });
-
-            System.out.println("BUSCANDO CLIENTE POR NOME");
-            clientes.buscarPorNome("Cli").forEach(System.out::println);
-
-            System.out.println("Deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            todosClientes = clientes.obterTodos();
-            if (todosClientes.isEmpty()) {
-                System.out.println("nenhum cliente encontrado");
-            } else {
-                todosClientes.forEach(System.out::println);
-            }
+//
+//            System.out.println("ATUALIZANDO CLIENTES");
+//            todosClientes.forEach(c -> {
+//                c.setNome(c.getNome() + " atualizado");
+//                clientesRespositorio.atualizar(c);
+//            });
+//
+//            System.out.println("BUSCANDO CLIENTE POR NOME");
+//            clientesRespositorio.buscarPorNome("Cli").forEach(System.out::println);
+//
+//            System.out.println("Deletando clientes");
+//            clientesRespositorio.obterTodos().forEach(c -> {
+//                clientesRespositorio.deletar(c);
+//            });
+//
+//            todosClientes = clientesRespositorio.obterTodos();
+//            if (todosClientes.isEmpty()) {
+//                System.out.println("nenhum cliente encontrado");
+//            } else {
+//                todosClientes.forEach(System.out::println);
+//            }
 
         };
     }
