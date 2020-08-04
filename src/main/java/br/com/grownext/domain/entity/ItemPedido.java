46b4b584b3d06022e23149838a8808b4ca21fa4f
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-08-04 07:00:28 -0300

    feat: migration db to mysql

--- src/main/java/br/com/grownext/domain/entity/ItemPedido.java
+++ src/main/java/br/com/grownext/domain/entity/ItemPedido.java
@@ -21,7 +21,7 @@ import javax.persistence.Table;
 public class ItemPedido {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
